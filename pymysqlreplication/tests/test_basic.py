d09aa271962e90c50a115c0b9e2836e3b65f5ed9
Julien Duponchelle <julien@duponchelle.info>
2012-09-21 22:12:20 +0200

    Test delete event

--- pymysqlreplication/tests/test_basic.py
+++ pymysqlreplication/tests/test_basic.py
@@ -43,6 +43,35 @@ class TestBinLogStreamReader(base.PyMySQLReplicationTestCase):
         self.assertEqual(event.event.values[0], 1)        
         self.assertEqual(event.event.values[1], "Hello World")        
 
+    def test_delete_row_event(self):
+        query = "CREATE TABLE test (id INT NOT NULL AUTO_INCREMENT, data VARCHAR (50) NOT NULL, PRIMARY KEY (id))"
+        self.execute(query)
+        query = "INSERT INTO test (data) VALUES('Hello World')"
+        self.execute(query)
+
+        self.resetBinLog()
+        
+        query = "DELETE FROM test WHERE id = 1"
+        self.execute(query)
+        self.execute("COMMIT")
+
+        #RotateEvent
+        self.stream.fetchone()
+        #FormatDescription
+        self.stream.fetchone()
+
+        #QueryEvent for the BEGIN
+        self.stream.fetchone()
+
+        event = self.stream.fetchone()
+        self.assertIsInstance(event.event, BinLogTableMapEvent)
+
+        event = self.stream.fetchone()
+        self.assertEqual(event.event_type, DELETE_ROWS_EVENT)        
+        self.assertIsInstance(event.event, DeleteRowsEvent)
+        self.assertEqual(event.event.values[0], 1)        
+        self.assertEqual(event.event.values[1], "Hello World")   
+
 __all__ = ["TestBinLogStreamReader"]
 
 if __name__ == "__main__":
