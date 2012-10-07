5b10334ae1c73364c7cd3f038742d1bf20d8a589
Julien Duponchelle <julien@duponchelle.info>
2012-10-07 15:46:12 +0200

    Usage example with redis

---
+++ examples/redis_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Update a redis server cache when an evenement is trigger
+# in MySQL replication log
+#
+
+import redis
+r = redis.Redis()
+
+from pymysqlreplication import BinLogStreamReader
+from pymysqlreplication.row_event import *
+
+mysql_settings = {'host': '127.0.0.1', 'port': 3306, 'user': 'root', 'passwd': ''}
+
+
+stream = BinLogStreamReader(connection_settings = mysql_settings,
+                           only_events = [DeleteRowsEvent, WriteRowsEvent, UpdateRowsEvent])
+
+for binlogevent in stream:
+    prefix = "%s:%s:" % (binlogevent.schema, binlogevent.table)
+
+    for row in binlogevent.rows:
+        if isinstance(binlogevent, DeleteRowsEvent):
+            r.delete(prefix + str(vals["id"]))
+        elif isinstance(binlogevent, UpdateRowsEvent):
+            vals = row["after_values"]
+            r.hmset(prefix + str(vals["id"]), vals)
+        elif isinstance(binlogevent, WriteRowsEvent):
+            vals = row["values"]
+            r.hmset(prefix + str(vals["id"]), vals)
+
+stream.close()
+
+
+
