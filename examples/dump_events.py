af80ae075c719997f6f1780247fd93a7ed524dfb
Julien Duponchelle <julien@duponchelle.info>
2012-11-09 21:48:24 +0100

    Document blocking option

--- examples/dump_events.py
+++ examples/dump_events.py
@@ -10,7 +10,8 @@ mysql_settings = {'host': '127.0.0.1', 'port': 3306, 'user': 'root', 'passwd': '
 
 
 #server_id is your slave identifier. It should be unique
-stream = BinLogStreamReader(connection_settings = mysql_settings, server_id = 3)
+#blocking: True if you want to block and wait for the next event at the end of the stream
+stream = BinLogStreamReader(connection_settings = mysql_settings, server_id = 3, blocking = True)
 
 for binlogevent in stream:
     binlogevent.dump()
