287e5d229f6f6a16e9bde534b796935e0b395d1a
Bartek Ogryczak <bartek@eventbrite.com>
2015-06-27 20:44:59 -0700

    Further (micro)optimization: Moving popular column types to top of if/elif, so they can shortcircuit earlier.

--- pymysqlreplication/column.py
+++ pymysqlreplication/column.py
@@ -21,36 +21,16 @@ class Column(object):
         self.collation_name = column_schema["COLLATION_NAME"]
         self.character_set_name = column_schema["CHARACTER_SET_NAME"]
         self.comment = column_schema["COLUMN_COMMENT"]
-        self.unsigned = False
+        self.unsigned = column_schema["COLUMN_TYPE"].find("unsigned") != -1
         self.type_is_bool = False
-        if column_schema["COLUMN_KEY"] == "PRI":
-            self.is_primary = True
-        else:
-            self.is_primary = False
+        self.is_primary = column_schema["COLUMN_KEY"] == "PRI"
 
-        if column_schema["COLUMN_TYPE"].find("unsigned") != -1:
-            self.unsigned = True
-        if self.type == FIELD_TYPE.VAR_STRING or \
-                self.type == FIELD_TYPE.STRING:
-            self.__read_string_metadata(packet, column_schema)
-        elif self.type == FIELD_TYPE.VARCHAR:
+        if self.type == FIELD_TYPE.VARCHAR:
             self.max_length = struct.unpack('<H', packet.read(2))[0]
-        elif self.type == FIELD_TYPE.BLOB:
-            self.length_size = packet.read_uint8()
-        elif self.type == FIELD_TYPE.GEOMETRY:
-            self.length_size = packet.read_uint8()
-        elif self.type == FIELD_TYPE.NEWDECIMAL:
-            self.precision = packet.read_uint8()
-            self.decimals = packet.read_uint8()
         elif self.type == FIELD_TYPE.DOUBLE:
             self.size = packet.read_uint8()
         elif self.type == FIELD_TYPE.FLOAT:
             self.size = packet.read_uint8()
-        elif self.type == FIELD_TYPE.BIT:
-            bits = packet.read_uint8()
-            bytes = packet.read_uint8()
-            self.bits = (bytes * 8) + bits
-            self.bytes = int((self.bits + 7) / 8)
         elif self.type == FIELD_TYPE.TIMESTAMP2:
             self.fsp = packet.read_uint8()
         elif self.type == FIELD_TYPE.DATETIME2:
@@ -60,6 +40,21 @@ class Column(object):
         elif self.type == FIELD_TYPE.TINY and \
                 column_schema["COLUMN_TYPE"] == "tinyint(1)":
             self.type_is_bool = True
+        elif self.type == FIELD_TYPE.VAR_STRING or \
+                self.type == FIELD_TYPE.STRING:
+            self.__read_string_metadata(packet, column_schema)
+        elif self.type == FIELD_TYPE.BLOB:
+            self.length_size = packet.read_uint8()
+        elif self.type == FIELD_TYPE.GEOMETRY:
+            self.length_size = packet.read_uint8()
+        elif self.type == FIELD_TYPE.NEWDECIMAL:
+            self.precision = packet.read_uint8()
+            self.decimals = packet.read_uint8()
+        elif self.type == FIELD_TYPE.BIT:
+            bits = packet.read_uint8()
+            bytes = packet.read_uint8()
+            self.bits = (bytes * 8) + bits
+            self.bytes = int((self.bits + 7) / 8)
 
     def __read_string_metadata(self, packet, column_schema):
         metadata = (packet.read_uint8() << 8) + packet.read_uint8()
