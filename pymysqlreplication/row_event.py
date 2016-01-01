af256c535f33ef9b7e81db86ecc9bd31e7531e9a
jffifa <jffifa@gmail.com>
2016-01-01 15:39:55 +0800

    bugfix for decoding values of columns in charset "utf8mb4"

--- pymysqlreplication/row_event.py
+++ pymysqlreplication/row_event.py
@@ -5,6 +5,7 @@ import decimal
 import datetime
 
 from pymysql.util import byte2int
+from pymysql.charset import charset_to_encoding
 
 from .event import BinLogEvent
 from .constants import FIELD_TYPE
@@ -194,7 +195,7 @@ class RowsEvent(BinLogEvent):
     def __read_string(self, size, column):
         string = self.packet.read_length_coded_pascal_string(size)
         if column.character_set_name is not None:
-            string = string.decode(column.character_set_name)
+            string = string.decode(charset_to_encoding(column.character_set_name))
         return string
 
     def __read_bit(self, column):
