3c0c7cbdcd82d46d209c9dc1137c184ac707cc5f
Xie Zhenye <xiezhenye@gmail.com>
2016-08-16 10:20:06 +0800

    fix tests for zero datetime type

--- pymysqlreplication/tests/test_data_type.py
+++ pymysqlreplication/tests/test_data_type.py
@@ -246,7 +246,7 @@ class TestDataType(base.PyMySQLReplicationTestCase):
         create_query = "CREATE TABLE test (id INTEGER, test DATE, test2 DATE);"
         insert_query = "INSERT INTO test (id, test2) VALUES(1, '0000-01-21')"
         event = self.create_and_insert_value(create_query, insert_query)
-        self.assertEqual(event.rows[0]["values"]["test"], None)
+        self.assertEqual(event.rows[0]["values"]["test"], 0)
         self.assertEqual(event.rows[0]["values"]["test2"], None)
 
     def test_zero_month(self):
@@ -254,7 +254,7 @@ class TestDataType(base.PyMySQLReplicationTestCase):
         create_query = "CREATE TABLE test (id INTEGER, test DATE, test2 DATE);"
         insert_query = "INSERT INTO test (id, test2) VALUES(1, '2015-00-21')"
         event = self.create_and_insert_value(create_query, insert_query)
-        self.assertEqual(event.rows[0]["values"]["test"], None)
+        self.assertEqual(event.rows[0]["values"]["test"], 0)
         self.assertEqual(event.rows[0]["values"]["test2"], None)
 
     def test_zero_day(self):
@@ -262,7 +262,7 @@ class TestDataType(base.PyMySQLReplicationTestCase):
         create_query = "CREATE TABLE test (id INTEGER, test DATE, test2 DATE);"
         insert_query = "INSERT INTO test (id, test2) VALUES(1, '2015-05-00')"
         event = self.create_and_insert_value(create_query, insert_query)
-        self.assertEqual(event.rows[0]["values"]["test"], None)
+        self.assertEqual(event.rows[0]["values"]["test"], 0)
         self.assertEqual(event.rows[0]["values"]["test2"], None)
 
     def test_time(self):
