8015b64c6b7ad18a939de8e02c73e04bed772338
sagi yosefia <sagi@sagis-MacBook-Pro.local>
2018-07-30 10:39:37 +0300

    cr fix

--- pymysqlreplication/utils.py
+++ pymysqlreplication/utils.py
@@ -2,14 +2,12 @@ import re
 from pkg_resources import parse_version
 
 
-class Utils:
-    def is_checksum_supported(self, server_version):
+def is_checksum_supported(server_version):
+    version_number = \
+        parse_version(re.findall('^([\d\.]+)', server_version)[0])
+    if parse_version('5.6.1') <= version_number or \
+            (("mariadb" in server_version.lower()) and
+             (parse_version('5.3') <= version_number)):
+            return True
 
-        version_number = \
-            parse_version(re.findall('^([\d\.]+)', server_version)[0])
-        if parse_version('5.6.1') <= version_number or \
-                (("mariadb" in server_version.lower()) and
-                 (parse_version('5.3') <= version_number)):
-                return True
-
-        return False
+    return False
