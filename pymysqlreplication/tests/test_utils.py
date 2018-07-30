118dac06af741ac94952e95d3280e88d6bebc7f7
sagi yosefia <sagi@sagis-MacBook-Pro.local>
2018-07-30 12:49:01 +0300

    fix bug

--- pymysqlreplication/tests/test_utils.py
+++ pymysqlreplication/tests/test_utils.py
@@ -8,19 +8,19 @@ class TextUtils(unittest.TestCase):
     def test_is_checksum_supported(self):
         # regular supported mysql version
         server_version = '5.7.17-log'
-        assert utils.is_checksum_supported(server_version) == True
+        assert utils.is_checksum_supported(server_version) is True
 
         # mariaDB supported mysql version
         server_version = '10.2.11-MariaDB-10.2.11+maria~jessie-log'
-        assert utils.is_checksum_supported(server_version) == True
+        assert utils.is_checksum_supported(server_version) is True
 
         # regular unsupported mysql version
         server_version = '5.3.1-log'
-        assert utils.is_checksum_supported(server_version) == False
+        assert utils.is_checksum_supported(server_version) is False
 
         # mariaDB unsupported mysql version
         server_version = '5.2.11-MariaDB'
-        assert utils.is_checksum_supported(server_version) == False
+        assert utils.is_checksum_supported(server_version) is False
 
         server_version = '5.2.11-mariadb'
-        assert utils.is_checksum_supported(server_version) == False
+        assert utils.is_checksum_supported(server_version) is False
