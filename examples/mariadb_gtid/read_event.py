24eeeefab344d2026f2c1109f109d6a2b053ce91
cucuridas@gamil.com <3310223@naver.com>
2023-07-27 15:23:48 +0900

    Changing the port number of 'MARIADB_SETTINGS'

--- examples/mariadb_gtid/read_event.py
+++ examples/mariadb_gtid/read_event.py
@@ -6,7 +6,7 @@ from pymysqlreplication.row_event import WriteRowsEvent, UpdateRowsEvent, Delete
 
 MARIADB_SETTINGS = {
     "host": "127.0.0.1",
-    "port": 4306,
+    "port": 3306,
     "user": "replication_user",
     "passwd": "secret123passwd",
 }
