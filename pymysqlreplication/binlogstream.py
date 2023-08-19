c186c82e64dfd28f18b6e050589fdf8905ddaec9
davinc71998 <davinc71998@gmail.com>
2023-08-19 15:08:13 +0900

    fix: set both variables 'replica_uuid' and 'slave_uuid' to MySQL for version compatibility

--- pymysqlreplication/binlogstream.py
+++ pymysqlreplication/binlogstream.py
@@ -301,7 +301,8 @@ class BinLogStreamReader(object):
 
         if self.slave_uuid:
             cur = self._stream_connection.cursor()
-            cur.execute("set @slave_uuid= '%s'" % self.slave_uuid)
+            cur.execute("SET @slave_uuid = %s, @replica_uuid = %s",
+                        (str(self.slave_uuid), str(self.slave_uuid)))
             cur.close()
 
         if self.slave_heartbeat:
