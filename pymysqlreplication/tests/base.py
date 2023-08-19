ab6d4eb28acb4743c7c17e6bdc23714dbec32257
heehehe <heeseon0315@gmail.com>
2023-08-19 15:39:39 +0900

    test: add isMySQL8014AndMore

--- pymysqlreplication/tests/base.py
+++ pymysqlreplication/tests/base.py
@@ -65,6 +65,11 @@ class PyMySQLReplicationTestCase(base):
         version = float(self.getMySQLVersion().rsplit('.', 1)[0])
         return version >= 8.0
 
+    def isMySQL8014AndMore(self):
+        version = float(self.getMySQLVersion().rsplit(".", 1)[0])
+        version_detail = int(self.getMySQLVersion().rsplit(".", 1)[1])
+        return version >= 8.0 and version_detail >= 14
+
     def isMariaDB(self):
         if self.__is_mariaDB is None:
             self.__is_mariaDB = "MariaDB" in self.execute("SELECT VERSION()").fetchone()[0]
