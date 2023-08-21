39dc6bb0bcc922a83915a59101cea9434c2bf550
mirageoasis <kimhw0820@naver.com>
2023-08-21 17:58:00 +0900

    fixed : fixed test_basic.py test_allowed_event_list method

--- pymysqlreplication/tests/test_basic.py
+++ pymysqlreplication/tests/test_basic.py
@@ -27,9 +27,9 @@ class TestBasicBinLogStreamReader(base.PyMySQLReplicationTestCase):
         return [GtidEvent]
 
     def test_allowed_event_list(self):
-        self.assertEqual(len(self.stream._allowed_event_list(None, None, False)), 17)
-        self.assertEqual(len(self.stream._allowed_event_list(None, None, True)), 16)
-        self.assertEqual(len(self.stream._allowed_event_list(None, [RotateEvent], False)), 16)
+        self.assertEqual(len(self.stream._allowed_event_list(None, None, False)), 20)
+        self.assertEqual(len(self.stream._allowed_event_list(None, None, True)), 19)
+        self.assertEqual(len(self.stream._allowed_event_list(None, [RotateEvent], False)), 19)
         self.assertEqual(len(self.stream._allowed_event_list([RotateEvent], None, False)), 1)
 
     def test_read_query_event(self):
