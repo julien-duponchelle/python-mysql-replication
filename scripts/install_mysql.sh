ca130cc4adf393a9bef623d0bfffa877cf2939e8
heehehe <heeseon0315@gmail.com>
2023-08-25 16:52:29 +0900

    remove: delete mysql8-related settings

--- scripts/install_mysql.sh
+++ scripts/install_mysql.sh
@@ -14,13 +14,10 @@ docker pull percona:$VERSION
 # Cleanup old mysql datas
 rm -rf /var/ramfs/mysql/
 rm -rf /var/ramfs/mysql-ctl/
-rm -rf /var/ramfs/mysql8/
 mkdir /var/ramfs/mysql/
 mkdir /var/ramfs/mysql-ctl/
-mkdir /var/ramfs/mysql8/
 chmod 777 /var/ramfs/mysql/
 chmod 777 /var/ramfs/mysql-ctl/
-chmod 777 /var/ramfs/mysql8/
 rm -rf /var/run/mysqld/
 mkdir /var/run/mysqld/
 chmod 777 /var/run/mysqld/
@@ -59,18 +56,6 @@ docker run --publish 3307:3306 \
 	$CTL_OPTIONS
 #	-v /var/run/mysqld/:/var/run/mysqld/\
 
-EIGHT_OPTIONS="$OPTIONS --server-id=3"
-EIGHT_OPTIONS="$EIGHT_OPTIONS --socket=/var/run/mysqld/mysqld8.sock"
-EIGHT_OPTIONS="$EIGHT_OPTIONS --datadir=/var/ramfs/mysql8/"
-EIGHT_OPTIONS="$EIGHT_OPTIONS --pid-file=/var/lib/mysql/mysql8.pid"
-
-docker run --publish 3308:3306 \
-	-d --name eight \
-	-e MYSQL_ALLOW_EMPTY_PASSWORD=yes\
-	-v /var/ramfs/mysql8/:/var/ramfs/mysql8/\
-	percona:8.0\
-	$EIGHT_OPTIONS
-
 for i in $(seq 0 40); do
 	sleep 1;
 	mysql -u root --host=127.0.0.1 --port=3306 -e 'SELECT VERSION();'
@@ -87,18 +72,8 @@ for i in $(seq 0 40); do
 	fi
 done
 
-for i in $(seq 0 40); do
-	sleep 1;
-	mysql -u root --host=127.0.0.1 --port=3308 -e 'SELECT VERSION();'
-	if [ $? -eq 0 ]; then
-		break 2;
-	fi
-done
-
 docker logs master
 docker logs ctl
-docker logs eight
 
 mysql -u root --host=127.0.0.1 --port=3306 -e 'CREATE DATABASE pymysqlreplication_test;'
 mysql -u root --host=127.0.0.1 --port=3307 -e "CREATE DATABASE pymysqlreplication_test;"
-mysql -u root --host=127.0.0.1 --port=3308 -e "CREATE DATABASE pymysqlreplication_test;"
