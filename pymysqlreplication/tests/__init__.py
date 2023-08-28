6fb0b2e1a7c83776b45c0d3ae4c922a2eae23439
Soul Lee <alus20x@gmail.com>
2023-08-28 13:57:40 +0900

    fix: Remove python2 compatibility

--- pymysqlreplication/tests/__init__.py
+++ pymysqlreplication/tests/__init__.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import unittest
 from pymysqlreplication.tests.test_basic import *
 from pymysqlreplication.tests.test_data_type import *
 from pymysqlreplication.tests.test_data_objects import *
 
 if __name__ == "__main__":
-    if sys.version_info < (2, 7):
-        import unittest2 as unittest
-    else:
-        import unittest
     unittest.main()
