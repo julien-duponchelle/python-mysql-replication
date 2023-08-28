0ddeb2cf54d24495120b8e9f263e5eb4f436b0a9
Soul Lee <alus20x@gmail.com>
2023-08-28 16:44:24 +0900

    fix: F811 Redefinition of unused `gtid` from line 3

--- examples/mariadb_gtid/read_event.py
+++ examples/mariadb_gtid/read_event.py
@@ -1,6 +1,6 @@
 import pymysql
 
-from pymysqlreplication import BinLogStreamReader, gtid
+from pymysqlreplication import BinLogStreamReader
 from pymysqlreplication.event import RotateEvent, MariadbGtidEvent, MariadbAnnotateRowsEvent, MariadbBinLogCheckPointEvent
 from pymysqlreplication.row_event import WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent
 
