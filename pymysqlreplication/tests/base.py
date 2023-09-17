7d070c46cb3f23c9ff093a5b56105b53647e64f5
mjs <munmun2004@naver.com>
2023-09-17 22:28:41 +0900

    test add percona8

--- pymysqlreplication/tests/base.py
+++ pymysqlreplication/tests/base.py
@@ -160,3 +160,27 @@ class PyMySQLReplicationMariaDbTestCase(PyMySQLReplicationTestCase):
         bin_log_basename = cursor.fetchone()[0]
         bin_log_basename = bin_log_basename.split("/")[-1]
         return bin_log_basename
+
+class PyMySQLReplicationPercona8TestCase(PyMySQLReplicationTestCase):
+    def setUp(self):
+        # default
+        self.database = {
+            "host": os.environ.get("MYSQL_8_0") or "localhost",
+            "user": "root",
+            "passwd": "",
+            "port": int(os.environ.get("MYSQL_8_0_PORT") or 3309),
+            "use_unicode": True,
+            "charset": "utf8",
+            "db": "pymysqlreplication_test",
+        }
+
+        self.conn_control = None
+        db = copy.copy(self.database)
+        db["db"] = None
+        self.connect_conn_control(db)
+        self.execute("DROP DATABASE IF EXISTS pymysqlreplication_test")
+        self.execute("CREATE DATABASE pymysqlreplication_test")
+        db = copy.copy(self.database)
+        self.connect_conn_control(db)
+        self.stream = None
+        self.resetBinLog()
