aa62a03625847310047e309b93f0572c59c5b19d
mirageoasis <kimhw0820@naver.com>
2023-10-20 11:18:48 +0900

    fixed : temporary on test_ignore_decode_errors

--- pymysqlreplication/tests/test_basic.py
+++ pymysqlreplication/tests/test_basic.py
@@ -748,7 +748,9 @@ class TestMultipleRowBinLogStreamReader(base.PyMySQLReplicationTestCase):
         )
         self.stream.close()
         self.execute("CREATE TABLE test (data VARCHAR(50) CHARACTER SET utf8mb4)")
-        self.execute(f"INSERT INTO test (data) VALUES ('{problematic_unicode_string}')")
+        self.execute_with_args(
+            "INSERT INTO test (data) VALUES (%s)", (problematic_unicode_string)
+        )
         self.execute("COMMIT")
 
         # Initialize with ignore_decode_errors=False
