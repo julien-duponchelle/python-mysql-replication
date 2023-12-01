14fb3ef3c9674d1e0887d4931d410690c70303ad
taehun.kim <taehun.kim2@hyosung.com>
2023-12-01 17:27:22 +0900

    Add properties in gtid event
    Motivation:
    GtidEvent is aleady implemented but there are missing 6 properties.
    If we respect these properties, user can use GtidEvent with more convenient.
    
    Motivation:
    - Fix(event.py): Add two properties.
    - Add(test_basic.py): Add test for GtidEvent.
    
    Result:
    for now on, user can use read_immediate_commit_timestamp and read_original_commit_timestamp properties.

--- pymysqlreplication/tests/test_basic.py
+++ pymysqlreplication/tests/test_basic.py
@@ -1472,6 +1472,37 @@ class TestRowsQueryLogEvents(base.PyMySQLReplicationTestCase):
         event = self.stream.fetchone()
         self.assertIsInstance(event, RowsQueryLogEvent)
 
+class TestGtidEvent(base.PyMySQLReplicationTestCase):
+    def setUp(self):
+        super(TestGtidEvent, self).setUp()
+        self.execute("SET SESSION binlog_rows_query_log_events=1")
+
+    def tearDown(self):
+        self.execute("SET SESSION binlog_rows_query_log_events=0")
+        super(TestGtidEvent, self).tearDown()
+
+    def test_gtid_event(self):
+        self.stream.close()
+        self.stream = BinLogStreamReader(
+            self.database,
+            server_id=1024,
+            only_events=[GtidEvent],
+        )
+        self.execute(
+            "CREATE TABLE IF NOT EXISTS test (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))"
+        )
+        gtid_event = self.stream.fetchone()
+
+        self.assertIsInstance(gtid_event, GtidEvent)
+        self.assertIsInstance(gtid_event.event_type, int)
+        self.assertIsInstance(gtid_event.sid, bytes)
+        self.assertIsInstance(gtid_event.gno, int)
+        self.assertIsInstance(gtid_event.lt_type, int)
+        self.assertIsInstance(gtid_event.last_committed, int)
+        self.assertIsInstance(gtid_event.sequence_number, int)
+        self.assertEqual(gtid_event.sequence_number, 1)
+        self.assertIsInstance(gtid_event.immediate_commit_timestamp, bytes)
+        self.assertIsInstance(gtid_event.original_commit_timestamp, bytes)
 
 class TestLatin1(base.PyMySQLReplicationTestCase):
     def setUp(self):
