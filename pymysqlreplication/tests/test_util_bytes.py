4be8cc150cc0aac6a38c860e8ec78ea119f63d74
roy0424 <roy0424@naver.com>
2023-12-07 01:43:40 +0900

    add annotations on test codes

--- pymysqlreplication/tests/test_util_bytes.py
+++ pymysqlreplication/tests/test_util_bytes.py
@@ -1,24 +1,34 @@
 from pymysqlreplication.util import *
 from pymysqlreplication.tests.base import PyMySQLReplicationTestCase
 
+"""
+These tests aim to cover some potential input scenarios, including valid inputs, edge cases, and error handling.
+This approach ensures that every line of the corresponding function is executed under test conditions, 
+thereby improving the coverage.
+"""
+
 
 class TestIsDataShort(PyMySQLReplicationTestCase):
     def test_data_is_shorter(self):
-        data = bytearray([0x01])
+        # Test with data shorter than expected.
+        data = bytearray([0x01])  # 1-byte data
         expected_length = 2
         self.assertTrue(is_data_short(data, expected_length))
 
     def test_data_is_equal_length(self):
-        data = bytearray([0x01, 0x00])
+        # Test with data equal to expected length.
+        data = bytearray([0x01, 0x00])  # 2-byte data
         expected_length = 2
         self.assertFalse(is_data_short(data, expected_length))
 
     def test_data_is_longer(self):
-        data = bytearray([0x01, 0x00, 0x02])
+        # Test with data longer than expected.
+        data = bytearray([0x01, 0x00, 0x02])  # 3-byte data
         expected_length = 2
         self.assertFalse(is_data_short(data, expected_length))
 
     def test_data_is_empty(self):
+        # Test with empty data.
         data = bytearray([])
         expected_length = 1
         self.assertTrue(is_data_short(data, expected_length))
@@ -26,12 +36,14 @@ class TestIsDataShort(PyMySQLReplicationTestCase):
 
 class TestDecodeCount(PyMySQLReplicationTestCase):
     def test_small_format(self):
+        # Test with 2-byte input and small format.
         data = bytearray([0x01, 0x00])
         is_small = True
         result = decode_count(data, is_small)
         self.assertEqual(result, 1)
 
     def test_large_format(self):
+        # Test with 4-byte input and large format.
         data = bytearray([0x01, 0x00, 0x00, 0x00])
         is_small = False
         result = decode_count(data, is_small)
@@ -40,11 +52,13 @@ class TestDecodeCount(PyMySQLReplicationTestCase):
 
 class TestDecodeUint(PyMySQLReplicationTestCase):
     def test_valid_input(self):
+        # Test with a known 2-byte input.
         data = bytearray([0x01, 0x00])
         result = decode_uint(data)
         self.assertEqual(result, 1)
 
     def test_short_data(self):
+        # Test with a 1-byte input, which is less than expected.
         data = bytearray([0x01])
         result = decode_uint(data)
         self.assertEqual(result, 0)
@@ -57,19 +71,29 @@ class TestDecodeUint(PyMySQLReplicationTestCase):
 
 class TestDecodeVariableLength(PyMySQLReplicationTestCase):
     def test_single_byte(self):
-        data = bytearray([0x05])
+        # Test with a single byte where the high bit is not set (indicating the end)
+        data = bytearray([0x05])  # 5 with the high bit not set
         length, pos = decode_variable_length(data)
         self.assertEqual(length, 5)
         self.assertEqual(pos, 1)
 
     def test_multiple_bytes(self):
+        # Test with multiple bytes
+        # 0x81 -> 1 with the high bit set, indicating more bytes
+        # 0x01 -> 1 with the high bit not set, indicating the end
+        # Combined value is 1 + (1 << 7) = 129
         data = bytearray([0x81, 0x01])
         length, pos = decode_variable_length(data)
         self.assertEqual(length, 129)
         self.assertEqual(pos, 2)
 
     def test_max_length(self):
-        data = bytearray([0x80, 0x80, 0x80, 0x80, 0x01])
+        # Test with the maximum length (5 bytes)
+        # This will test the boundary condition of the loop in the function
+        data = bytearray(
+            [0x80, 0x80, 0x80, 0x80, 0x01]
+        )  # Each 0x80 has the high bit set, 0x01 does not
+        # The value is 1 << (7 * 4) = 2**28
         length, pos = decode_variable_length(data)
         self.assertEqual(length, 2**28)
         self.assertEqual(pos, 5)
@@ -82,7 +106,7 @@ class TestParseUint16(PyMySQLReplicationTestCase):
         self.assertEqual(result, 1)
 
     def test_different_input(self):
-        data = bytearray([0xFF, 0x00])
+        data = bytearray([0xFF, 0x00])  # Represents the unsigned integer 255
         result = parse_uint16(data)
         self.assertEqual(result, 255)
 
@@ -106,24 +130,28 @@ class TestLengthEncodedInt(PyMySQLReplicationTestCase):
 
 class TestDecodeTime(PyMySQLReplicationTestCase):
     def test_midnight(self):
-        data = bytearray([0x00] * 8)
+        data = bytearray([0x00] * 8)  # Represents 00:00:00
         result = decode_time(data)
         self.assertEqual(result, datetime.time(0, 0, 0))
 
     def test_valid_time(self):
-        data = bytearray([0x00, 0x00, 0x00, 0xC0, 0x18, 0x01, 0x00, 0x00])
+        data = bytearray(
+            [0x00, 0x00, 0x00, 0xC0, 0x18, 0x01, 0x00, 0x00]
+        )  # Represents 17:35:00
         result = decode_time(data)
         self.assertEqual(result, datetime.time(17, 35, 0))
 
 
 class TestDecodeDatetime(PyMySQLReplicationTestCase):
     def test_zero_datetime(self):
-        data = bytearray([0x00] * 8)
+        data = bytearray([0x00] * 8)  # Represents 0000-00-00 00:00:00
         result = decode_datetime(data)
         self.assertEqual(result, "0000-00-00 00:00:00")
 
     def test_valid_datetime(self):
-        data = bytearray([0x00, 0x00, 0x00, 0xC0, 0x18, 0x9B, 0xB1, 0x19])
+        data = bytearray(
+            [0x00, 0x00, 0x00, 0xC0, 0x18, 0x9B, 0xB1, 0x19]
+        )  # Represents 2023-11-13 17:35:00
         expected_datetime = datetime.datetime(
             year=2023, month=11, day=13, hour=17, minute=35, second=0
         )
