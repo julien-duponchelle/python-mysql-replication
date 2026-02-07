5d2692eff97be0e1f1dbee2b1bc1a5f5043aa918
Julien Duponchelle <julien@duponchelle.info>
2026-02-07 12:54:40 +0100

    Bump version to 1.0.15

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ class TestCommand(Command):
         unittest.main(tests, argv=sys.argv[:1])
 
 
-version = "1.0.14"
+version = "1.0.15"
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
